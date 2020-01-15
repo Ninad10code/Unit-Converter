40581a25322880323d84287dea63a744896e2564
Ninad Dadmal <47479487+Ninad10code@users.noreply.github.com>
2020-01-15 14:13:49 +0530

    Update converter.cpp

--- converter.cpp
+++ converter.cpp
@@ -2,14 +2,18 @@
 #include<fstream>
 #include <bits/stdc++.h> 
 using namespace std;
-int q1,q2;
+
 float value;
+
+//Structure created for each unit as a node
 struct units
 {
     float value[5],result[5];
     string converted_to[5];
     int child_node;
 } link[100];
+
+//Function named universal created to execute unit conversion
 float universal(float value,int flag,float lib_int,int trig)
 {
     if(flag==1)
@@ -48,32 +52,30 @@ float universal(float value,int flag,float lib_int,int trig)
     
 }
 
+//Main function
 int main()
 {
-    int flag,l=1,count=0,l_pin,pin[28][10],calls=0,trig=0;
-    int trace,l_odd=1,l_even=0,p_count=0,index,arr_size[120];
+    int flag,l=1,count=0,trig=0;
+    int trace;
     float container;
-    string  units[10],lib[40];
+    string lib[40];//To store the library.txt file
     string u[6],lib_str,initial;
     cout<<"CONVERTER\n";
     cout<<"Enter the value you want to convert\n";
     cin>>value;
     container=value;
     cout<<"Enter the units you want to convert from\n";
-    cin>>u[0]>>u[1];
+    cin>>u[0]>>u[1];//Source unit and target unit
     initial=u[0];
     
 
-    string b[120][12];
-    string d[30][2];
-    string a[120];
-    string c[30];
-    fstream file;
-    fstream allocate;
+    //Reading library.txt file
+   
+    
     fstream data;
     string library_file,library_path; 
-    string word, t, filename; 
-    string path,assign;
+     
+    
     int i=0,stop_count=0,length_count=0,word_count=0;
     library_file = "library.txt";
     data.open(library_file.c_str());
@@ -85,32 +87,43 @@ int main()
         count++;
     }
     file.close();
-    
+    //library.txt closed
     
 
     int found=0,unfound=0;
     
     
     i=0;
-    int reset=0,j=1,k=4,counter=0,search[30][2],track=0,check_count=0,execute=1,m=0,n=0,p=0,z=0,q=0,o=1;
+    int j=1,k=4,track=0,check_count=0,execute=1,m=0,n=0,p=0,z=0,q=0,o=1;
     float lib_int;
     string check[10];
+
+    //If source unit and target unit are found in library.txt it gets executed in the for loop
     for (int i = 0; i < (count/5) && track!=1; i++)
     {
         if(u[0]==lib[j] && u[1]==lib[k])
         {
+            /*flag is set to 1 when the higher unit is converted to lower unit
+            eg To convert km to m flag will set to 1 as km>m*/
             track=1;
             flag=1;
+            /*lib_str stores the conversion factor from lib[k-1]
+             which was read from library.txt file*/
             lib_str=lib[k-1];
             stringstream num (lib_str);
             num >> lib_int;
-           
+            /*The lib_int stores the lib_str value as a floating number
+            after converting the string value into floating value*/
             link[0].result[0]=universal(value,flag,lib_int,trig);
+            /*link[0].result[0] stores the converted value after
+             calling the universal function*/
             cout<<container<<initial<<" = "<<link[0].result[0]<<u[1]<<"\n";
             
         }
         else if (u[0]==lib[k] && u[1]==lib[j])
         {
+            /*flag is set to 0 when the lower unit is converted to higher unit
+            eg To convert m to km flag will set to 0 as m<km*/
             track=1;
             flag=0;
             lib_str=lib[k-1];
@@ -130,22 +143,34 @@ int main()
         
         
     }
+    //If no direct conversion of units exists in library.txt then track=0
+    
+    /*check[] string is used to check whether the unit was already converted to some unit before
+    which prevents the unit getting converted to the previous unit*/
+
     check[0]=u[0];
     
     check_count++;
    
-    
+    /*link[].child_node states the number of possible conversion from a particular unit
+    eg: cm can be converted to m, ft and in. Therefore the nodes for cm will be 3*/
     link[0].child_node=0;
   
     if(track==0)
     {
         while(found!=1)
         {
-            
+          //While loop executes untill found is not equal to 1  
             j=1;k=4;
             for (int i = 0; i < (count/5) && trace!=1; i++)
             {
-                
+                /*The for loop executes to find all possible path of conversion
+                 eg suppose in library.txt it is given that cm can be converted
+                 to m, ft and in then the for loop finds all such possible conversion*/
+           
+                /*As in library.txt it is given that
+                 1 km = 1000 m
+                 here lib[j] represent km*/
                 if (u[0]==lib[j])
                 {
                     
@@ -154,11 +179,13 @@ int main()
                         if(check[i]==lib[j+3])
                         {
                             execute=0;
-                            
+                         /*This is used to backtrack that whether the unit was converted earlier
+                         to prevent the converted unit from getting converted to previous units*/   
                             
                         }
                     }
-                  
+
+                    //If the unit has not been converted before then it gets executed
                     if (execute==1)
                     {
                         flag=1;
@@ -171,7 +198,7 @@ int main()
                         link[m].converted_to[n]=lib[j+3];
                        
                         check[o]=lib[j+3];
-                       
+                       //Every new converted unit gets added into check[o]
                         check_count++;
                        
                         o++;
@@ -190,6 +217,9 @@ int main()
                     
                     
                 }
+                /*As in library.txt it is given that
+                 1 km = 1000 m
+                 here lib[k] represent m*/
                 else if (u[0]==lib[k])
                 {
                  
@@ -244,12 +274,18 @@ int main()
             link[m].child_node=0;
             if (p<link[z].child_node)
             {
+                
                 if (value==0)
                 {
                     cout<<"Cannot be calculated\n";
                     found=1;
                 }
-                
+
+                /*u[0] is updated to the value of child node
+                eg earlier u[0] was km and then it was converted
+                to m which is its child node hence then the new 
+                u[0] value will be set to m*/
+
                 u[0]=link[z].converted_to[q];
                 
                 value=link[z].result[q];
@@ -268,13 +304,18 @@ int main()
                     cout<<"Cannot be calculated\n";
                     found=1;
                 }
+
+                /*When all child nodes are used a new parent node is created
+                eg: km has one child node that was m
+                then m has one child node that was cm
+                here m became a parent node for cm*/
                 q=0;
                 p=0;
                 u[0]=link[z].converted_to[q];
                 z++;
               
             }
-            
+            /*For every new node new structure gets generated*/
             
         
         }
